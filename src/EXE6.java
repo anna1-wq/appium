7a6fb5b6b2bc11e079ab4a99fb6df3b136b79910
anna1-wq <nnbrams@gmail.com>
2019-10-30 19:45:57 +0300

    EXE6

---
+++ src/EXE6.java
@@ -0,0 +1,98 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.*;
+import java.net.URL;
+import java.util.List;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+public class EXE6 {
+    private AppiumDriver driver;
+
+    @Before
+    public void SetUp() throws Exception
+    {
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","10");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/admin/Desktop/AppiumAuto/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+    }
+
+    @After
+    public void tearDown()
+    {
+
+        driver.quit();
+    }
+
+    @Test
+    public void testAssertTitles()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' search input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "appium",
+                "Cannot find 'Search…' search input",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Appium']"),
+                "Cannot find article",
+                15
+        );
+
+        assertElementPresent("There is no title element");
+
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver , timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+    public void assertElementPresent(String error_message)
+    {
+        Assert.assertEquals(
+                "org.wikipedia:id/view_page_title_text",
+                "org.wikipedia:id/view_page_title_text"
+        );
+
+    }
+}
